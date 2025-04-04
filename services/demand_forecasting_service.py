ceff6229654b3142079bebce323e320800c3bb08
MARKovka <santa.umer@gmail.com>
2025-04-04 20:42:51 +0500

    Groupping by productname on demand_forecasting_service.py

--- services/demand_forecasting_service.py
+++ services/demand_forecasting_service.py
@@ -3,26 +3,11 @@
 import numpy as np
 from services.models.demand_forecasting_model import build_demand_forecasting_model
 
-# class DemandForecastingService:
-#     def __init__(self):
-#         # Допустим, модель принимает последовательность с 1 временным шагом и 1 признаком
-#         self.model = build_demand_forecasting_model(input_shape=(1, 1))
-#
-#     def forecast(self, data):
-#         # Из колонки 'Quantity_Sold' формируем входной массив для модели
-#         X = data['Quantity_Sold'].values.reshape(-1, 1, 1)
-#         predicted = self.model.predict(X)
-#         data['Predicted_Demand'] = predicted.flatten()
-#         return data
-
 class DemandForecastingService:
     def __init__(self):
-        # Модель принимает последовательность с 1 временным шагом и 1 признаком (агрегированное Quantity_Sold)
         self.model = build_demand_forecasting_model(input_shape=(1, 1))
 
     def forecast(self, data):
-        # Группируем данные по названию продукта,
-        # суммируя 'Quantity_Sold' и усредняя другие числовые показатели.
         grouped = data.groupby(
             'Product_Name',
             as_index=False
@@ -43,7 +28,7 @@ class DemandForecastingService:
             'Season': lambda x: x.mode()[0] if not x.mode().empty else x.iloc[0],
             'Region': lambda x: x.mode()[0] if not x.mode().empty else x.iloc[0]
         })
-        # Формируем входной массив для модели из агрегированного 'Quantity_Sold'
+
         X = grouped['Quantity_Sold'].values.reshape(-1, 1, 1)
         predicted = self.model.predict(X)
         grouped['Predicted_Demand'] = predicted.flatten()
